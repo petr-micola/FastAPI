c332808b18fc9f59b11fba2e546e26504539f06a
Petr Mičola <77784828+petr-micola@users.noreply.github.com>
2023-03-12 16:21:44 +0100

    Metoda PATCH pro úpravu záznamu.

--- main.py
+++ main.py
@@ -46,7 +46,21 @@ async def add_item(item: Item):
     j = open('data.json', 'w')
     j.write(json.dumps(data))
     j.close()
+    return item
 
+
+@app.patch('/ranking/{item_id}')
+async def update_item(item_id: int, item: Item):
+    j = open('data.json', 'r')
+    data = json.load(j)
+    data[item_id]['team'] = item.team
+    data[item_id]['points'] = item.points
+    data[item_id]['link'] = item.link
+    j.close()
+
+    j = open('data.json', 'w')
+    j.write(json.dumps(data))
+    j.close()
     return item
 
 
@@ -60,5 +74,4 @@ async def delete_item(item_id: int):
     j = open('data.json', 'w')
     j.write(json.dumps(data))
     j.close()
-
-    return {'message': 'Deleted'}
+    return {'message': f'Deleted item with id {item_id}'}
