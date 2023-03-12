1291369f41489da959937aaf7c6d82626768f90d
Petr Mičola <77784828+petr-micola@users.noreply.github.com>
2023-03-12 15:20:58 +0100

    Metoda POST pro přidání záznamu.

--- main.py
+++ main.py
@@ -1,25 +1,51 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
 import json
 
-from fastapi import FastAPI
 from scraper import Scraper
 
 app = FastAPI()
 s = Scraper()
+s.scrapedata()
+
+
+class Item(BaseModel):
+    id: int
+    team: str
+    points: int
+    link: str
+
+
+@app.get('/')
+async def root():
+    return {"message": "Hello World"}
 
 
 @app.get('/ranking')
 async def read_items():
-    s.scrapedata()
     j = open('data.json', 'r')
     data = json.load(j)
     j.close()
     return data
 
 
-@app.get('/ranking/{id}')
-async def read_item(id):
-    s.scrapedata()
+@app.get('/ranking/{item_id}')
+async def read_item(item_id: int):
     j = open('data.json', 'r')
     data = json.load(j)
     j.close()
-    return data[int(id)]
+    return data[int(item_id)]
+
+
+@app.post('/ranking/')
+async def add_item(item: Item):
+    j = open('data.json', 'r')
+    data = json.load(j)
+    data.append(dict(item))
+    j.close()
+
+    j = open('data.json', 'w')
+    j.write(json.dumps(data))
+    j.close()
+
+    return item
